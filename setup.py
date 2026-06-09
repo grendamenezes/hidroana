76fbae1289e6cafb1d2e1dcdad63b6aae1c7a482
Grenda Menezes <126720472+grendamenezes@users.noreply.github.com>
2026-06-08 21:44:51 -0300

    Bump version from 0.1.1 to 0.1.2

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ classifiers=[
  
 setup(
   name='hidroana',
-  version='0.1.1',
+  version='0.1.2',
   description='Download and process hydrological data from ANA (Brazil)',
   long_description=long_description,
   long_description_content_type='text/markdown',
